f72e04690a9e989a7de46f9fa3c5b37e29ebc205
Liquid-Tungston <b410396@gm.tnfsh.tn.edu.tw>
2026-03-29 15:02:08 +0800

    refactor: <type>(<scope>): <subject>
    description

--- CleanCodeHW/calculate_pi.py
+++ CleanCodeHW/calculate_pi.py
@@ -1,18 +1,17 @@
 import random
 # Constant settings
 RADIUS = 1
-NUM_POINTS = 1000000
+TOTAL = 1000000
 SQ_AREA = (RADIUS * 2) ** 2
-inside_circle = 0
+CIRCLED = 0
 
 # Randomly generate points and count those inside the circle
-for _ in range(1000000):
+for _ in range(TOTAL):
     x = random.uniform(-RADIUS, RADIUS)
     y = random.uniform(-RADIUS, RADIUS)
     if x**2 + y**2 <= RADIUS**2:
-        inside_circle += 1
+        CIRCLED += 1
 
 # Estimate pi based on the number of points inside the circle
-est_pi = (inside_circle / NUM_POINTS) * SQ_AREA
-
-print(f"Estimated value of pi is: {est_pi}")
+EST_PI = (CIRCLED / TOTAL) * SQ_AREA
+print(f"Estimated value of pi is: {EST_PI}")
